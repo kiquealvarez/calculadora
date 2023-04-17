0421ffea124813729bb8ac2e44586cf15029d21f
Enrique Álvarez De Diego <enrique.alvarez.dediego95@gmail.com>
2023-04-17 19:59:58 +0200

    feat: añadida la librería local y actualizados los métodos

--- src/main/java/com/calculator/controller/ControllerCalculator.java
+++ src/main/java/com/calculator/controller/ControllerCalculator.java
@@ -1,20 +1,34 @@
 package com.calculator.controller;
 
+import io.corp.calculator.TracerImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/calculator")
 public class ControllerCalculator {
 
+  private final TracerImpl tracer = new TracerImpl();
+
   @GetMapping(value = "/addition")
-  public String addition(){
-    return "Estoy sumando";
+  public ResponseEntity<String> addition(@RequestParam(name = "firstNumber") final Integer firstNumber,
+      @RequestParam(name = "secondNumber") final Integer secondNumber) {
+    final int sum = firstNumber + secondNumber;
+    final String result = "Suma: " + firstNumber + " + " + secondNumber + " = " + sum;
+    tracer.trace(result);
+    return new ResponseEntity<>(result, HttpStatus.OK);
   }
 
   @GetMapping(value = "/subtract")
-  public String subtract(){
-    return "Estoy restando";
+  public ResponseEntity<String> subtract(@RequestParam(name = "firstNumber") final Integer firstNumber,
+      @RequestParam(name = "secondNumber") final Integer secondNumber) {
+    final int rest = firstNumber - secondNumber;
+    final String result = "Resta: " + firstNumber + " - " + secondNumber + " = " + rest;
+    tracer.trace(result);
+    return new ResponseEntity<>(result, HttpStatus.OK);
   }
 }
